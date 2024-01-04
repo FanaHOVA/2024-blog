658293aaf8fdb22ece097ebc1241569501250292
Alessio Fanelli <fanahova@gmail.com>
2024-01-04 00:09:11 -0800

    Add generative OG images

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const meta = {
-    title: "Alessio Fanelli's blog",
-    description: 'Random thoughts and ideas.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
-  }
-
   return (
     <Html lang="en">
       <Head>
         <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
--- theme.config.js
+++ theme.config.js
@@ -17,5 +17,18 @@ export default {
         }
       `}</style>
     </small>
-  )
+  ),
+  head: ({ title, meta }) => (
+    <>
+      <meta property="og:site_name" content={meta.title || "Alessio Fanelli's blog"} />
+      <meta property="og:description" content={meta.description || "Alessio Fanelli's blog"} />
+      <meta property="og:title" content={meta.title || "Alessio Fanelli's blog"} />
+      <meta property="og:image" content={meta.image || `https://dynamic-og-image-generator.vercel.app/api/generate?title=${encodeURIComponent(meta.title)}&author=Alessio%20Fanelli&avatar=https://pbs.twimg.com/profile_images/1588566693088927744/11P6gQdW_400x400.jpg&websiteUrl=alessiofanelli.com&theme=dracula`} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@fanahova" />
+      <meta name="twitter:title" content={meta.title || "Alessio Fanelli's blog"} />
+      <meta name="twitter:description" content={meta.description || "Alessio Fanelli's blog"} />
+      <meta property="twitter:image" content={meta.image || `https://dynamic-og-image-generator.vercel.app/api/generate?title=${encodeURIComponent(meta.title)}&author=Alessio%20Fanelli&avatar=https://pbs.twimg.com/profile_images/1588566693088927744/11P6gQdW_400x400.jpg&websiteUrl=alessiofanelli.com&theme=dracula`} />
+    </>
+  ),
 }
